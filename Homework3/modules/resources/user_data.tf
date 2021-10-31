a8fb614085d27a64150c17f462518a8f91d87659
yossi <yossi@hiredscore.com>
2021-10-31 23:25:59 +0200

    adding s3 log files

--- Homework3/modules/resources/user_data.tf
+++ Homework3/modules/resources/user_data.tf
@@ -7,6 +7,9 @@ sudo apt install nginx -y
 sed -i "s/nginx/Grandpa's Whiskey - $HOSTNAME.srv/g" /var/www/html/index.nginx-debian.html
 sed -i '15,23d' /var/www/html/index.nginx-debian.html
 service nginx restart
+########## Upload Access Log To S3 Now & Every Hour ##########
+echo "0 * * * * root aws s3 cp /var/log/nginx/access.log  s3://${var.purpose_tag}-${var.s3_logs_bucket_name}/$HOSTNAME-access.log" | tr [:upper:] [:lower:] | sudo tee -a /etc/crontab
+
 
 USERDATA
 }
